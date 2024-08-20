61191be9410aa05a2adfb6ecbe3645d45a2187cd
Daniel <danieldafonseca@sapo.pt>
2024-08-20 11:13:31 +0100

    testing and fixing

--- SimpleWebServer/src/TcpServer.java
+++ SimpleWebServer/src/TcpServer.java
@@ -1,5 +1,4 @@
 
-
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -8,7 +7,6 @@ public class TcpServer {
 
     public static void main(String[] args) {
         int portNumber = 8081;
-
 
         try {
             ServerSocket serverSocket = new ServerSocket(portNumber);
@@ -20,26 +18,36 @@ public class TcpServer {
             String[] input = in.readLine().split(" ");
             String resource = input[1];
 
+            System.out.println(resource);
             if (resource.equals("/index.html")) {
-                File file = new File("src/rsc/index.html");
-                BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-                String html = "HTTP/1.0 200 Document Follows\r\n" +
-                        "Content-Type: text/html; charset=UTF-8\r\n" +
-                        "Content-Length:" + file.length() + " \r\n" +
-                        "\r\n";
-                String line;
-                String result = html;
-                while ((line = bufferedReader.readLine()) != null) {
-                    result += line + "\n";
+
+                File file = new File("SimpleWebServer/src/rsc/index.html");
+                try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+                    System.out.println("hello");
+                    String html = "HTTP/1.0 200 Document Follows\r\n" +
+                            "Content-Type: text/html; charset=UTF-8\r\n" +
+                            "Content-Length:" + file.length() + " \r\n" +
+                            "\r\n";
+                    String line;
+                    String result = html;
+
+
+                    while ((line = bufferedReader.readLine()) != null) {
+                        result += line + "\n";
+                    }// Read and process the file
+                    System.out.println(html);
+                    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                    out.println(result);
+                    clientSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-                System.out.println(html);
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                out.println(result);
-                clientSocket.close();
+
+
             }
 
             if (resource.equals("/logo.png")) {
-                File png = new File("src/rsc/logo.png");
+                File png = new File("SimpleWebServer/src/rsc/logo.png");
                 FileInputStream fileInputStream = new FileInputStream(png);
                 DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
                 byte[] buffer = new byte[1024];
@@ -59,7 +67,7 @@ public class TcpServer {
                 out.close();
             }
             if (!resource.equals("/logo.png") && !resource.equals("/index.html")) {
-                File file = new File("src/rsc/404.html");
+                File file = new File("SimpleWebServer/src/rsc/404.html");
                 BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
                 String notFound = "HTTP/1.0 404 Not Found\n" +
                         "Content-Type: text/html; charset=UTF-8\r\n" +
